237b860737f27d8d875022547c78d00d5a96725a
Abhilash Gowda P <abhilashgowda14012014@gmail.com>
2024-05-10 20:54:45 +0530

    updated version of the database

--- backend/db_config.js
+++ backend/db_config.js
@@ -2,13 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCk5hShcb50wgCsBCfg1CzpHf6CGHHyhXM",
-  authDomain: "tms-abhidigitech.firebaseapp.com",
-  projectId: "tms-abhidigitech",
-  storageBucket: "tms-abhidigitech.appspot.com",
-  messagingSenderId: "65666375515",
-  appId: "1:65666375515:web:c04e9e4c8c799b551cc47e",
-  measurementId: "G-P4XBCF32QH",
+
 };
 
 // Initialize Firebase
